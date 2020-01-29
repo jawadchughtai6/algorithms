851a27d274afab9bf653facec208a19cfd24ad1d
Jawad Chughtai <jawadchughtai6@mehanapp.com>
2020-01-29 17:06:05 +0300

    reverseint completed

--- reverseint/index.js
+++ reverseint/index.js
@@ -9,9 +9,9 @@
 //   reverseInt(-90) === -9
 
 function reverseInt(n) {
-    let strNumber = n.toString();
+    let strNumber = n.toString();                   //convert n to a string and store it in a variable.
     let reverseNumber = '';
-    for(let i=strNumber.length-1; i>=0; i--){
+    for(let i=strNumber.length-1; i>=0; i--){       //start looping from behind. Go back. store each value in a new string.
         reverseNumber = reverseNumber + strNumber[i];
     }
     return parseInt(reverseNumber, 10);             //parseInt is a function that converts string to integer. 10 is to convert it to dec.
